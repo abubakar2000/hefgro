b44a974c587b699c23d3422c7eadc721d18705d8
Abubakar Mughal <64458681+abubakar2000@users.noreply.github.com>
2022-04-17 20:45:44 +0500

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigatorComponent } from './shared/navigator/navigator.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TabsComponent } from './shared/tabs/tabs.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { CarouselComponent } from './shared/carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigatorComponent,
+    TabsComponent,
+    LandingPageComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
